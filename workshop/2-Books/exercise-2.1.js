f1d9ba78c1c70ed16fe2e465b5fc5e47af486aa3
Arturo Chen <art.can.fly@gmail.com>
2020-06-25 15:14:46 -0400

    E2.1 done

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,23 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+  constructor(title, genre, author, isRead=false) {
+    this.title = title
+    this.genre = genre
+    this.author = author
+    this.isRead = isRead
+  }
+}
 
-console.log(book1, book2, book3, book4, book5);
+const book1 = new Book('Beautiful Disaster', 'Romance', 'Jamie McGuire', false)
+const book2 = new Book('Divergent', 'Science Fiction', 'Veronica Roth', true)
+const book3 = new Book('The Hunger Games', 'Science Fiction', 'Suzanne Collins')
+const book4 = new Book('The Chronicles of Narnia', 'Fantasy', 'C.S. Lewis')
+const book5 = new Book('The WAy of Kings', 'Fantasy', 'Brandon Sanderson', true)
+
+console.log(JSON.stringify(book1, null, 2));
+console.log(JSON.stringify(book2, null, 2));
+console.log(JSON.stringify(book3, null, 2));
+console.log(JSON.stringify(book4, null, 2));
+console.log(JSON.stringify(book5, null, 2));

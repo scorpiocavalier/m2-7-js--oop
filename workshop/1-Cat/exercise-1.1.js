abd7930e4cbc54780cf65b793849e661b4594637
Arturo Chen <art.can.fly@gmail.com>
2020-06-25 11:11:05 -0400

    E1.1 done

--- workshop/1-Cat/exercise-1.1.js
+++ workshop/1-Cat/exercise-1.1.js
@@ -14,11 +14,22 @@
 // }
 
 class Cat {
-  // Add code here
+  constructor(species) {
+    this.species = species
+    this.tiredness = 0
+    this.hunger = 0
+    this.loneliness = 0
+    this.happiness = 0
+  }
 }
 
 // B) Instantiate a cat called 'boots' with the Cat class.
+let boots = new Cat('cat')
 
 // C) What do you see when you console.log(boots)?
+console.log(boots)
+// Cat {species: "cat", tiredness: 0, hunger: 0, loneliness: 0, happiness: 0}
 
 // D) What if I want to output just boots' species?
+console.log(boots.species)
+// cat
